8abd509cc8ff8e5fa47321fa252656c308b369ce
DyingZhang <DyingZhang@shd.com>
2022-07-12 00:58:48 +0800

    SecondCommit

--- Paperwork-Week01/src/CardLost.java
+++ Paperwork-Week01/src/CardLost.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
  * @version v1.0
  * @time 2022-07-11-23:44
  * @Description
+ * 对于Lost和其子类的说明：
+ * 通常情况下，我们应用数据库连接池读取相应数据
+ * 但由于工作室进度，我在此用代码块的形式显式将数据赋值给lostArray数组中
+ * 这就导致如果Lost中不赋值，其子类继承它的实例方法时，Lost中没有数据而报空指针异常
+ * 故将Lost两个方法抽象化，令其子类实现，但这种方式代码复用率高，故不提倡
+ * 但是目前阶段解决问题我采用这种方式，等进度到之后会优化。
  */
 public class CardLost extends Lost {
 
@@ -30,7 +36,15 @@ public class CardLost extends Lost {
 
     @Override
     public void sortLost(){
-        Arrays.sort(lostArray);
+        for (int i = 0; i < lostArray.length-1; i++) {
+            for (int j = i+1; j < lostArray.length; j++) {
+                if (lostArray[i].compareTo(lostArray[j]) > 0){
+                    OneLost oneLost = lostArray[i];
+                    lostArray[i] = lostArray[j];
+                    lostArray[j] = oneLost;
+                }
+            }
+        }
     }
 
     @Override
--- Paperwork-Week01/src/Lost.java
+++ Paperwork-Week01/src/Lost.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
  * @version v1.0
  * @time 2022-07-11-23:00
  * @Description
+ * 对于Lost和其子类的说明：
+ * 通常情况下，我们应用数据库连接池读取相应数据
+ * 但由于工作室进度，我在此用代码块的形式显式将数据赋值给lostArray数组中
+ * 这就导致如果Lost中不赋值，其子类继承它的实例方法时，Lost中没有数据而报空指针异常
+ * 故将Lost两个方法抽象化，令其子类实现，但这种方式代码复用率高，故不提倡
+ * 但是目前阶段解决问题我采用这种方式，等进度到之后会优化。
  */
 public abstract class Lost {
     private OneLost[] lostArray;
